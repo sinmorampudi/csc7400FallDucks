2846a84286fb87419286f2176f4766e23ba80e5e
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:15:25 -0500

    Create QuackBehaviour.java

---
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/QuackBehaviour.java
@@ -0,0 +1,16 @@
+* Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 9
+ * @author Sindhuja Morampudi,divya kasu
+ * Date: 2017-11-28
+ */
+package edu.fitchugstate.csc7400.y2017fall.duckpond;
+
+/**
+ * Interface for sound behaviour
+ *
+ */
+public interface QuackBehaviour {
+
+	public void quack();
+}
