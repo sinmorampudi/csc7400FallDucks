a286149bbba559af7d0e327b48bf9f51dd8982ae
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:08:53 -0500

    Update DuckType.java

--- src/edu/fitchugstate/csc7400/y2017fall/duckpond/DuckType.java
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/DuckType.java
@@ -2,7 +2,7 @@
  * Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 9
- * 
+ * @author Sindhuja Morampudi,divya kasu
  * Date: 2017-11-28
  */
 package edu.fitchugstate.csc7400.y2017fall.duckpond;
@@ -21,12 +21,15 @@ public interface DuckType {
    *  Shows a flying animation
    */
   public void fly();
+  
+  
+  
 
   /** 
    *  Outputs a quacking sound
    */
   public void quack();
-
+  
   /** 
    *  Displays a swimming animation
    */
