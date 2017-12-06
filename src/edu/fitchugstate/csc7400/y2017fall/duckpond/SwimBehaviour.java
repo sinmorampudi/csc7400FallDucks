c9ab4e20dec733930a32a7b9950ed3d310004b96
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:19:05 -0500

    Create SwimBehaviour.java

---
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/SwimBehaviour.java
@@ -0,0 +1,18 @@
+* Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 9
+ * @author Sindhuja Morampudi,divya kasu
+ * Date: 2017-12-06
+ */
+package edu.fitchugstate.csc7400.y2017fall.duckpond;
+
+import external.GIF;
+
+/**
+ * Interface for Swim functionality
+ *
+ */
+public interface SwimBehaviour {
+
+	public void swim(GIF Swim);
+}
