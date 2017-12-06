95b7ad0cd3b2a160a3a2870489fddc8be6bae78d
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:14:51 -0500

    Create Quack.java

---
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/Quack.java
@@ -0,0 +1,19 @@
+* Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 9
+ * @author Sindhuja Morampudi,divya kasu
+ * Date: 2017-12-06
+ */
+package edu.fitchugstate.csc7400.y2017fall.duckpond;
+
+/**
+ * implements QuackBehaviour to output quack
+ *
+ */
+public class Quack implements QuackBehaviour{
+
+	public void quack()
+	{
+		System.out.println("quack");
+	}
+}
