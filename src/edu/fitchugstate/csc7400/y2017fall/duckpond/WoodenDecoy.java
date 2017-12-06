5dad3ea4b8eaf447aa5cacedfc9de9861f92a0b7
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:19:37 -0500

    Update WoodenDecoy.java

--- src/edu/fitchugstate/csc7400/y2017fall/duckpond/WoodenDecoy.java
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/WoodenDecoy.java
@@ -2,7 +2,7 @@
  * Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 9
- * 
+ * @author Sindhuja Morampudi,divya kasu
  * Date: 2017-11-28
  */
 package edu.fitchugstate.csc7400.y2017fall.duckpond;
@@ -13,23 +13,30 @@ package edu.fitchugstate.csc7400.y2017fall.duckpond;
 public class WoodenDecoy extends Duck {
 
   /** 
-   *  Creates a new wooden decoy with appropriate bitmaps and GIFs
+   *  Creates a new wooden decoy with appropriate bitmaps,GIFs and creates objects for appropriate behaviour classes
    */
   public WoodenDecoy() {
-    super("decoy_still.bmp", null, "decoy_swim.gif");
+    super(BitMapFactory.createBitmap("decoy_still.bmp"), GifFactory.createGif("null"), GifFactory.createGif("decoy_swim.gif"));
+    
+    
+    flyBehaviour = new NotFlying();
+	quackBehaviour = new NoQuack();
+	swimBehaviour = new Swim();
   }
+  
+  
 
   /** 
    *  Wooden decoys don't fly.
    */
-  public void fly() {
-    System.out.println("Decoy still swims");
-  }
+  //public void fly() {
+  //  System.out.println("Decoy still swims");
+  //}
 
   /** 
    *  Wooden decoys don't quack
    */
-  public void quack() {
-    System.out.println("Decoy is silent");
-  }
+  //public void quack() {
+   // System.out.println("Decoy is silent");
+ // }
 }
