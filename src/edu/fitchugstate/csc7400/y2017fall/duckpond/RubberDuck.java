4b60500c067ab0c7d441dc508ac1cad525619206
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-12-06 17:16:35 -0500

    Update RubberDuck.java

--- src/edu/fitchugstate/csc7400/y2017fall/duckpond/RubberDuck.java
+++ src/edu/fitchugstate/csc7400/y2017fall/duckpond/RubberDuck.java
@@ -1,8 +1,7 @@
-/**
- * Class: Object-Oriented Design and Analysis
+* Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 9
- * 
+ * @author Sindhuja Morampudi,divya kasu
  * Date: 2017-11-28
  */
 package edu.fitchugstate.csc7400.y2017fall.duckpond;
@@ -10,26 +9,36 @@ package edu.fitchugstate.csc7400.y2017fall.duckpond;
 /** 
  *  Rubber duck for pond
  */
+
 public class RubberDuck extends Duck {
 
   /** 
    *  Creates a new rubber duck
    */
   public RubberDuck() {
-	super("rubber_duck_still.bmp", null, "rubber_duck_swim.gif");
+	super(BitMapFactory.createBitmap("rubber_duck_still.bmp"), GifFactory.createGif(null), GifFactory.createGif("rubber_duck_swim.gif"));
+
+	//setFlyBehaviour(new NotFlying());
+    //setSoundBehaviour(new Squeak());
+    //setSwimBehaviour(new Swim());
+	flyBehaviour = new NotFlying();
+	quackBehaviour = new Squeak();
+	swimBehaviour=new Swim();
   }
+
+
 
   /** 
    *  Rubber duck don't fly
    */
-  public void fly() {
-    System.out.println("Rubber duck continues to swim");
-  }
+ // public void fly() {
+ //   System.out.println("Rubber duck continues to swim");
+ // }
 
   /** 
    *  Squeaks like a rubber duck.
    */
-  public void quack() {
-    System.out.println("Squeak");
-  }
+ // public void quack() {
+  //  System.out.println("Squeak");
+ // }
 }
